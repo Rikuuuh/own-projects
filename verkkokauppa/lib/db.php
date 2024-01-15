70902a7da0606afba01beb7332a8fdb3deedd9cc
Riku <77975239+Rikuuuh@users.noreply.github.com>
2024-01-15 17:58:46 +0200

    Update db.php

--- verkkokauppa/lib/db.php
+++ verkkokauppa/lib/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$sql = new mysqli("localhost", "riku", "Lolled123!", "sakky_riku");
+$sql = new mysqli("localhost", "riku", "", "sakky_riku");
 
 if ($sql->connect_errno) {
 	die("Ongelmia tietokannan kanssa " . $sql->connect_error);

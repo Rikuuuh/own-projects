5cfc104d5800a9ca5756993ed40b40ea158919c7
Rikuuuh <77975239+Rikuuuh@users.noreply.github.com>
2024-04-17 12:24:01 +0300

    ++ kuvat & slider

--- portfolio/script.js
+++ portfolio/script.js
@@ -14,4 +14,27 @@ function closeModal(modalId) {
     var modal = document.getElementById(modalId);
     modal.style.display = "none";
   }
+
+  var slideIndexes = {
+    modal1: 1,
+    modal2: 1,
+    modal3: 1
+  };
+  
+  function plusSlides(n, modalId) {
+    showSlides(slideIndexes[modalId] += n, modalId);
+  }
+  
+  function showSlides(n, modalId) {
+    var i;
+    var slides = document.querySelectorAll("#" + modalId + " .slide");
+    if (n > slides.length) { slideIndexes[modalId] = 1; }
+    if (n < 1) { slideIndexes[modalId] = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndexes[modalId] - 1].style.display = "block";  
+  }
+  
+  
   
